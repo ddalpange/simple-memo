3ef09c1d17eb62b4bd718aed919808632b74e292
DESKTOP-UGABN8O\김요한 <yohan.kim@traport.com>
2017-07-23 01:03:57 +0900

    chapter2 - 메모 리스트 페이지 퍼블리싱

--- src/pages/memo-list/memo-list.ts
+++ src/pages/memo-list/memo-list.ts
@@ -1,24 +1,25 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the MemoListPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import { MemoCreatePage } from './../memo-create/memo-create';
+import { MemoDetailPage } from './../memo-detail/memo-detail';
+@IonicPage()
 
 @Component({
   selector: 'page-memo-list',
   templateUrl: 'memo-list.html',
 })
-export class MemoListPage {
 
+export class MemoListPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-
   ionViewDidLoad() {
     console.log('ionViewDidLoad MemoListPage');
   }
-
+  onClickViewMemoDetail() {
+    this.navCtrl.push(MemoDetailPage);
+  }
+  onClickCreateMemo() {
+    this.navCtrl.push(MemoCreatePage);
+  }
 }
