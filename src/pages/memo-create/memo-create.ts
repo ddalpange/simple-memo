068d60e638b47ec8543ca234df453544647125f9
DESKTOP-UGABN8O\김요한 <yohan.kim@traport.com>
2017-07-23 01:06:59 +0900

    chapter2 - 메모 만들기 페이지 퍼블리싱

--- src/pages/memo-create/memo-create.ts
+++ src/pages/memo-create/memo-create.ts
@@ -1,24 +1,30 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the MemoCreatePage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-
+@IonicPage()
 @Component({
   selector: 'page-memo-create',
   templateUrl: 'memo-create.html',
 })
-export class MemoCreatePage {
 
+export class MemoCreatePage {
+  title: string;
+  contents: string;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MemoCreatePage');
+  ngOnInit() {
   }
 
+  onChangeTitle(event: KeyboardEvent) {
+    this.title = event.target['value'];
+  }
+  
+  onChangeContents(event: KeyboardEvent) {
+    this.contents = event.target['value'];
+  }
+
+  onSaveMemo() {
+    this.navCtrl.pop();
+  }
 }
