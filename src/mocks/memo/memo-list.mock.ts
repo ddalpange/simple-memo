da27bb81c691c3c977ddbc9dfd2f3b70d67e6887
DESKTOP-UGABN8O\김요한 <yohan.kim@traport.com>
2017-07-26 21:35:01 +0900

    chapter3: 메모 인터페이스, 목업 메모리스트 추가

---
+++ src/mocks/memo/memo-list.mock.ts
@@ -0,0 +1,19 @@
+import { Memo } from './../../models/memo/memo.interface';
+export const MEMOLIST: Memo[] = [
+    {
+        key: 0,
+        title: "제목 1",
+        contents: "본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문",
+        author: "ddalpange@gmail.com",
+        recentUpdatedDate: new Date(),
+        publishedDate: new Date(),
+    },
+    {
+        key: 1,
+        title: "제목 2",
+        contents: "본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문본문",
+        author: "ddalpange@gmail.com",
+        recentUpdatedDate: new Date(),
+        publishedDate: new Date(),
+    }
+]
---
+++ src/models/memo/memo.interface.ts
@@ -0,0 +1,8 @@
+export interface Memo {
+    key: number;
+    author: string;                 // 작성자
+    title: string;                  // 제목
+    contents: string;               // 본문
+    publishedDate: Date;           // 작성일
+    recentUpdatedDate: Date;      // 최근 수정일
+}
