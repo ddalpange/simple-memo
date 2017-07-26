002b4e22e79dd66da421b2c6ca464984f178eb2e
DESKTOP-UGABN8O\김요한 <yohan.kim@traport.com>
2017-07-26 21:34:10 +0900

    chapter2: 앱 모듈을 아이오닉v3 에 맞게 모듈 로딩방식으로 변경

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,40 +4,36 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+import { SignUpPageModule } from './../pages/sign-up/sign-up.module';
+import { SignInPageModule } from './../pages/sign-in/sign-in.module';
+import { MemoListPageModule } from './../pages/memo-list/memo-list.module';
+import { MemoDetailPageModule } from './../pages/memo-detail/memo-detail.module';
+import { MemoCreatePageModule } from './../pages/memo-create/memo-create.module';
+
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { SignInPage } from './../pages/sign-in/sign-in';
-import { SignUpPage } from '../pages/sign-up/sign-up';
-import { MemoDetailPage } from './../pages/memo-detail/memo-detail';
-import { MemoListPage } from './../pages/memo-list/memo-list';
-import { MemoCreatePage } from '../pages/memo-create/memo-create';
 
-function getPages() {
-  return [
-    MyApp,
-    SignInPage,
-    SignUpPage,
-    MemoListPage,
-    MemoDetailPage,
-    MemoCreatePage
-  ]
-}
 @NgModule({
-  declarations: getPages(),
+  declarations: [MyApp],
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule,
     IonicModule.forRoot(MyApp),
+    MemoCreatePageModule,
+    MemoDetailPageModule,
+    MemoListPageModule,
+    SignInPageModule,
+    SignUpPageModule
   ],
   bootstrap: [IonicApp],
-  entryComponents: getPages(),
+  entryComponents: [MyApp],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
   ]
 })
 export class AppModule {}
