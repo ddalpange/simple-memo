559082e6bd398e7f18df3f2b3b2adcd46e6a370c
김요한 <ddalpange@gmail.com>
2017-07-29 14:13:20 +0900

    chapter4: 로그인 기능 완성

--- src/pages/sign-in/sign-in.ts
+++ src/pages/sign-in/sign-in.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
 
 import { SignUpPage } from './../sign-up/sign-up';
 import { MemoListPage } from './../memo-list/memo-list';
@@ -13,12 +13,14 @@ import { AuthManagerProvider } from '../../providers/auth-manager/auth-manager';
 
 export class SignInPage {
   
-  emailAddress: string;
-  password: string;
+  emailAddress: string = '';
+  password: string = '';
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
+    public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController,
     public authManager: AuthManagerProvider
   ) {}
   ionViewDidLoad() {
@@ -26,18 +28,47 @@ export class SignInPage {
   }
   ngOnInit() {
   }
+
   onClickDDalpange() {
     window.location.href = "https://ddalpange.github.io";
   }
+
   onClickEmailLogIn() {
-    // this.afAuth.auth.signInAnonymously().then(res => console.log(res));
-    this.authManager.loginUser(this.emailAddress, this.password);
-    // this.navCtrl.setRoot(MemoListPage);    
+    let loader = this.loadingCtrl.create({
+      content: '로그인 중입니다 ..'
+    });
+
+    loader.present();
+
+    this.authManager.loginUser(this.emailAddress, this.password)
+      .then(user => {
+      console.log('성공!', user);
+      loader.dismiss();
+      this.navCtrl.setRoot(MemoListPage);
+    })
+    .catch(err => { 
+      console.error('실패!', err);
+      loader.dismiss();
+      const alert = this.getFailAlert(err.message);
+      alert.present();
+    });;
   }
+
   onClickFacebookLogin() {
-    console.log(this.emailAddress);
-    console.log(this.password);
+    const alert = this.getFailAlert('미완성 기능입니다.');
+    alert.present();
   }
+
+  getFailAlert(message: string) {
+    return this.alertCtrl.create({
+      title: 'failed..',
+      subTitle: message,
+      buttons: [{
+        text: '확인'
+      }]
+    });
+  }
+
   onClickSignUp() {
     this.navCtrl.push(SignUpPage);
   }
